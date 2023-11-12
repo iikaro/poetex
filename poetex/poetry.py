115e07aa41b96083bd8bb021ad0f1a78c293995f
Icaro <icaro@kelda.no>
2023-11-12 07:53:56 +0100

    docs: add documentation to methods inside poetry module

--- poetex/poetry.py
+++ poetex/poetry.py
@@ -5,12 +5,24 @@ SINGLE_LINE_SPACING = '\n'
 
 
 def load_file_contents(file_path: str) -> str:
+    """
+    Load contents of text file into plain string.
+    :param file_path: Path to file.
+    :return: String with file contents.
+    """
     with open(file_path, 'r', encoding="utf-8") as file:
         contents = file.read()
     return contents
 
 
 def load_poem(file_path: str, title_type: TitleType = TitleType.FIRST_LINE) -> Poem:
+    """
+    Load poem from text file (.txt) and store it in a Poem object.
+    :param file_path: Path to text file.
+    :param title_type: Enum stating how the title of the poem will be extracted from the text file. Default is
+    'first_line', i.e., first line of the text file will be used as title and removed from the final Poem object.
+    :return: Poem object.
+    """
     contents = load_file_contents(file_path)
     stanzas = [Stanza(verses=[Verse(text=verse) for verse in _get_verses(stanza)]) for stanza in _get_stanzas(contents)]
     title = Untitled
