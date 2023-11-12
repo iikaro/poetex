cc3293f998ff056bddb2c3f51b2a12291f13ca20
Icaro <icaro@kelda.no>
2023-11-12 08:21:49 +0100

    fix: fix tests to be discoverable by unittests command

---
+++ local_env.py
@@ -0,0 +1,5 @@
+import os.path
+
+ROOT = os.path.dirname(__file__)
+SOURCE_DIR = 'source'
+SOURCE = os.path.join(ROOT, SOURCE_DIR)
--- poetex/tests/poem_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from poetex.poetry import load_poem
-
-
-class TestLoadPoem(unittest.TestCase):
-    def test_T01_load_poem(self):
-        poem = load_poem(r'..\..\source\morning_song.txt')
-        self.assertEqual(len(poem.stanzas), 6)
