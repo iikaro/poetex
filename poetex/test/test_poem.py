b6fadfa9d023b2bfe6bfda38cb997befec30c3e1
Icaro <icaro@kelda.no>
2023-11-17 18:32:50 +0100

    feat: add method and test to compile latex PDF

--- poetex/test/test_poem.py
+++ poetex/test/test_poem.py
@@ -2,7 +2,7 @@ import unittest
 
 from local_env import SOURCE
 from poetex.file_manager.manager import get_list_of_files
-from poetex.latex.builder import populate_template, build
+from poetex.latex.builder import populate_template, build, compile_latex
 from poetex.poetry import load_poem
 
 
@@ -18,11 +18,8 @@ class TestLoadPoem(unittest.TestCase):
         poems = [load_poem(path) for path in paths]
         self.assertEqual(len(poems), 2)
 
-    def test_T03_test_poem_builder(self):
-        paths = get_list_of_files(SOURCE)
-        poems = [load_poem(path) for path in paths]
-        populate_template(poems)
-        build()
+    def test_T03_compile_latex(self):
+        compile_latex()
 
 
 if __name__ == "__main__":
