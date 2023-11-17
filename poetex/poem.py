ce4d3a3f8341ddba63727a4b82628d64131ccc92
Icaro <icaro@kelda.no>
2023-11-17 18:14:34 +0100

    fix: fix poem builder algorithm to not add extra line spacing after last stanza

--- poetex/poem.py
+++ poetex/poem.py
@@ -81,15 +81,40 @@ class Poem(PoetexBaseModel):
     def lines(self) -> int:
         return self.stanzas[0].lines
 
+    def __len__(self):
+        return len(self.stanzas)
+
+    def is_last_stanza(self, index: int) -> bool:
+        if index == len(self) - 1:
+            return True
+        return False
+
+    def is_last_verse(self, stanza_index, verse_index: int) -> bool:
+        if verse_index == len(self.stanzas[stanza_index].verses) - 1:
+            return True
+        return False
+
     def to_latex_verse(self) -> str:
         begin = "\\begin{verse}" + SINGLE_LINE_SPACING
         end = "\\end{verse}"
 
         poem_latex = [begin]
-        for stanza in self.stanzas:
-            for verse in stanza.verses:
-                poem_latex.append(verse.text + END_OF_VERSE + SINGLE_LINE_SPACING)
-            poem_latex.append(END_OF_STANZA)
+        for stanza_index, stanza in enumerate(self.stanzas):
+            for verse_index, verse in enumerate(stanza.verses):
+                # Add verse
+                poem_latex.append(verse.text)
+                # Add double line spacing LaTeX command if the poem continues to the next stanza (\\)
+                if not self.is_last_verse(stanza_index, verse_index):
+                    poem_latex.append(END_OF_VERSE)
+                # Add single line spacing to string (\n)
+                poem_latex.append(SINGLE_LINE_SPACING)
+
+            # Add LaTeX command to finish stanza (!)
+            if not self.is_last_stanza(stanza_index):
+                poem_latex.append(END_OF_STANZA)
+
+            # Add double line spacing (\n\n)
             poem_latex.append(DOUBLE_LINE_SPACING)
+
         poem_latex.append(end)
         return "".join(poem_latex)
