303386b072be97f1dc6366491f631925eca5abdf
Icaro <icaro@kelda.no>
2023-11-29 19:34:50 +0100

    feat: add poem data classes to store poem metadata

---
+++ poetex/database/dataclasses.py
@@ -0,0 +1,34 @@
+from poetex.poem import PoetexBaseModel, Language
+
+
+class PoemBibTexData(PoetexBaseModel):
+    author: str
+    title: str
+    date: str
+    book_title: str
+    publisher: str
+    year: str
+    editor: str
+    volume: str
+    number: str
+    series: str
+    pages: str
+    address: str
+    edition: str
+    month: str
+    note: str
+    key: str
+    source: str
+    url: str
+
+
+class PoemMetaData(PoetexBaseModel):
+    isbn_10: str
+    isbn_13: str
+    doi: str
+    genres: list[str]
+    language: Language
+
+
+class PoemData(PoemBibTexData, PoemMetaData):
+    ...
