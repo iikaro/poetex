346051de5c422e8b25c8981a8137f7220571ee18
Icaro <icaro@kelda.no>
2023-11-29 18:23:27 +0100

    docs: update readme and add custom 'make' command to regenerate documentation

--- docs/conf.py
+++ docs/conf.py
@@ -5,28 +5,42 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'poetex'
-copyright = '2023, Icaro Ostan'
-author = 'Icaro Ostan'
-release = '0.1.0'
+project = "poetex"
+copyright = "2023, Icaro"
+author = "Icaro"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_autodoc_typehints",
+    "sphinxcontrib.autodoc_pydantic",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+add_module_names = False
+autodoc_pydantic_model_show_json = True
+autodoc_pydantic_settings_show_json = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
+
+SPHINX_APIDOC_OPTIONS = [
+    "members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
+    "imported-members",
+]
